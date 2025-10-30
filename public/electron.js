90608b4ec7f6a459e960efe48a63da49822d71e2
Florian Grima <florian.grima@epitech.eu>
2025-10-30 18:50:28 +0100

    feat: can update the application

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,13 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
+
+// Configure auto-updater
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 function createWindow() {
   // Create the browser window
@@ -49,8 +54,87 @@ function createWindow() {
   });
 }
 
+// Auto-updater event handlers
+autoUpdater.on('checking-for-update', () => {
+  console.log('Checking for updates...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'Update Available',
+    message: `A new version (${info.version}) is available!`,
+    buttons: ['Download', 'Later'],
+    defaultId: 0
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.downloadUpdate();
+    }
+  });
+});
+
+autoUpdater.on('update-not-available', () => {
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'No Updates',
+    message: 'You are running the latest version.',
+    buttons: ['OK']
+  });
+});
+
+autoUpdater.on('error', (err) => {
+  console.error('Update error:', err);
+  dialog.showMessageBox(mainWindow, {
+    type: 'error',
+    title: 'Update Error',
+    message: 'An error occurred while checking for updates.',
+    detail: err.toString(),
+    buttons: ['OK']
+  });
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  const percent = Math.round(progressObj.percent);
+  console.log(`Download progress: ${percent}%`);
+  mainWindow.setProgressBar(percent / 100);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  mainWindow.setProgressBar(-1);
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'Update Ready',
+    message: `Version ${info.version} has been downloaded.`,
+    detail: 'The update will be installed when you quit the application.',
+    buttons: ['Restart Now', 'Later'],
+    defaultId: 0
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.quitAndInstall();
+    }
+  });
+});
+
+// Function to check for updates
+function checkForUpdates() {
+  if (!isDev) {
+    autoUpdater.checkForUpdates();
+  } else {
+    console.log('Auto-update is disabled in development mode');
+  }
+}
+
 // This method will be called when Electron has finished initialization
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  
+  // Check for updates on startup (after 3 seconds to let the app load)
+  if (!isDev) {
+    setTimeout(() => {
+      checkForUpdates();
+    }, 3000);
+  }
+});
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
@@ -83,6 +167,13 @@ const template = [
       {
         label: 'About Lexa Guard',
         role: 'about'
+      },
+      { type: 'separator' },
+      {
+        label: 'Check for Updates...',
+        click: () => {
+          checkForUpdates();
+        }
       },
       { type: 'separator' },
       {
